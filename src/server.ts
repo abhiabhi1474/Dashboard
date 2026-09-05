669423d2c55454957f0fd9e08369621b491c9c6d
abhiabhi1474 <abhishekas2004@gmail.com>
2026-09-05 16:38:14 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,12 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+app.set('trust proxy', true);
+
+const angularApp = new AngularNodeAppEngine({
+  trustProxyHeaders: true,
+  allowedHosts: ['employee-dashboard-o7cx.onrender.com']
+});
 
 /**
  * Example Express Rest API endpoints can be defined here.
